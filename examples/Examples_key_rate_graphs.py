6eeba4fe7f19e8346091cbec65b23e962e29992b
AnoukDpy <dupuyanouk@laposte.net>
2026-08-05 15:53:35 +0200

    Refactor key rate functions, add BBM92

--- examples/Examples_key_rate_graphs.py
+++ examples/Examples_key_rate_graphs.py
@@ -15,15 +15,16 @@ from fresqcos.channel import Channel
 
 ## Plot
 
-def key_rate_distance_km_bb84(min, max, values_number, source: Source, detector: Detector, channel: Channel, receiver: Receiver, correction_efficiency: float, title: str):
+def key_rate_distance_km_bb84(*, min, max, values_number, source: Source, detector: Detector, channel: FiberChannel, receiver: Receiver, correction_efficiency: float, title: str):
     x_values = np.linspace(min, max, values_number)
     y1_values = []
     for x in x_values:
-        protocol = BB84(source, detector, channel, receiver, correction_efficiency, x)
+        channel.distance_km = x
+        protocol = BB84(source=source, detector=detector, channel=channel, receiver=receiver, correction_efficiency=correction_efficiency)
         y1 = protocol.key_rate_decoy_state_inf_key()
         y1_values.append(y1)
 
-    plt.plot(x_values, y1_values, color = 'blue', label = "With active decoy state")
+    plt.plot(x_values, y1_values, color='blue', label="With active decoy state")
     plt.yscale('log')
     plt.xlabel("distance_km in km")
     plt.ylabel("Key rate in bpp")
@@ -32,15 +33,24 @@ def key_rate_distance_km_bb84(min, max, values_number, source: Source, detector:
     plt.grid(True)
     plt.show()
 
-def key_rate_distance_km_bbm92(min, max, values_number, source: Source, detector1: Detector,detector2: Detector, channel1: Channel,channel2: Channel, receiver1: Receiver,receiver2: Receiver, correction_efficiency: float, title: str):
+def key_rate_distance_km_pulsed_bbm92(*, min, max, values_number, source: Source, detector1: Detector, detector2: Detector, channel_1: FiberChannel, channel_2: FiberChannel, receiver1: Receiver, receiver2: Receiver, correction_efficiency: float, title: str):
     x_values = np.linspace(min, max, values_number)
+    x_axis = []
     y1_values = []
     for x in x_values:
-        protocol = BBM92(source, detector1,detector2, channel1,channel2, receiver1,receiver2, correction_efficiency, x,0)
+        channel_1.distance_km = x
+        if channel_2 is None:
+            ch2 = channel_1
+        else:
+            ch2 = channel_2
+
+        x_axis.append(channel_1.distance_km+ch_2.distance_km)
+
+        protocol = Pulsed_BBM92(source=source, detector1=detector1, channel_1=channel_1, channel_2 = ch_2, receiver1=receiver1, correction_efficiency=correction_efficiency, detector2=detector2, receiver2=receiver2)
         y1 = protocol.key_rate()
         y1_values.append(y1)
 
-    plt.plot(x_values, y1_values, color = 'red')
+    plt.plot(x_values, y1_values, color='red')
     plt.yscale('log')
     plt.xlabel("distance_km in km")
     plt.ylabel("Key rate in bpp")
@@ -49,16 +59,17 @@ def key_rate_distance_km_bbm92(min, max, values_number, source: Source, detector
     plt.grid(True)
     plt.show()
 
-def key_rate_loss_bb84(min, max, values_number, source: Source, detector: Detector, channel: Channel, receiver: Receiver, correction_efficiency: float, title: str):
+def key_rate_loss_bb84(*, min, max, values_number, source: Source, detector: Detector, channel: FiberChannel, receiver: Receiver, correction_efficiency: float, title: str):
     x_values = np.linspace(min, max, values_number)
-    horiz_axis = np.linspace(min*channel.loss_coef, max*channel.loss_coef, values_number)
+    horiz_axis = np.linspace(min * channel.loss_per_km, max * channel.loss_per_km, values_number)
     y1_values = []
     for x in x_values:
-        protocol = BB84(source, detector, channel, receiver, correction_efficiency, x)
+        channel.distance_km = x
+        protocol = BB84(source=source, detector=detector, channel=channel, receiver=receiver, correction_efficiency=correction_efficiency)
         y1 = protocol.key_rate_decoy_state_inf_key()
         y1_values.append(y1)
 
-    plt.plot(horiz_axis, y1_values, color = 'blue', label = "With active decoy state")
+    plt.plot(horiz_axis, y1_values, color='blue', label="With active decoy state")
     plt.yscale('log')
     plt.xlabel("Loss in dB")
     plt.ylabel("Key rate in bpp")
@@ -67,87 +78,112 @@ def key_rate_loss_bb84(min, max, values_number, source: Source, detector: Detect
     plt.grid(True)
     plt.show()
 
-def key_rate_loss_bbm92(min, max, values_number, source: Source, detector1: Detector,detector2: Detector, channel1: Channel,channel2: Channel, receiver1: Receiver,receiver2: Receiver, correction_efficiency: float, title: str):
+
+def key_rate_loss_pulsed_bbm92(*, min, max, values_number, source: Source, detector1: Detector, channel_1: FiberChannel, receiver1: Receiver, correction_efficiency: float, title: str, detector2: Optional[Detector] = None, channel_2: Optional[FiberChannel] = None, receiver2: Optional[Receiver] = None):
     x_values = np.linspace(min, max, values_number)
-    horiz_axis = np.linspace(min*channel1.loss_coef, max*channel1.loss_coef, values_number)
+    horiz_axis = []
     y1_values = []
+
     for x in x_values:
-        protocol = BBM92(source, detector1,detector2, channel1,channel2, receiver1,receiver2, correction_efficiency, x,0)
+        channel_1.distance_km = x
+        if channel_2 is None:
+            ch2 = channel_1
+        else:
+            ch2 = channel_2
+        horiz_axis.append(channel_1.distance_km * channel_1.loss_per_km + ch2.distance_km * ch2.loss_per_km)
+
+        y1_values.append(Pulsed_BBM92(source=source, detector1=detector1, detector2=detector2, channel_1=channel_1, channel_2=ch2, receiver1=receiver1, receiver2=receiver2, correction_efficiency=correction_efficiency).key_rate())
+
+    plt.plot(horiz_axis, y1_values, color='red')
+    plt.yscale('log')
+    plt.xlabel("Loss in dB")
+    plt.ylabel("Key rate in bits/s")
+    plt.title(title)
+    plt.grid(True)
+    plt.show()
+
+def key_rate_loss_bbm92_continuous(*, min, max, values_number, source: Source, detector1: Detector, channel_1: FiberChannel, receiver1: Receiver, correction_efficiency: float, title: str, coincidence_time: float, detector2: Optional[Detector] = None, channel_2: Optional[FiberChannel] = None, receiver2: Optional[Receiver] = None):
+    x_values = np.linspace(min, max, values_number)
+    horiz_axis = []
+    y1_values = []
+
+
+    for x in x_values:
+        channel_1.distance_km = x
+        if channel_2 is None:
+            ch2 = channel_1
+        else:
+            ch2 = channel_2
+
+        protocol = BBM92_continuous_wave_pumped_source(source=source, detector1=detector1, detector2=detector2, channel_1=channel_1, channel_2=ch2, receiver1=receiver1, receiver2=receiver2, correction_efficiency=correction_efficiency, coincidence_time=coincidence_time)
         y1 = protocol.key_rate()
         y1_values.append(y1)
+        horiz_axis.append(-10 * np.log10((protocol.heralding_efficiency_1_x() + protocol.heralding_efficiency_1_z()) / 2 * (protocol.heralding_efficiency_2_x() + protocol.heralding_efficiency_2_z()) / 2))
 
-    plt.plot(horiz_axis, y1_values, color = 'red')
+    plt.plot(horiz_axis, y1_values, color='red')
     plt.yscale('log')
     plt.xlabel("Loss in dB")
-    plt.ylabel("Key rate in bpp")
+    plt.ylabel("Key rate in bits/s")
     plt.title(title)
-    plt.legend()
     plt.grid(True)
     plt.show()
 
 
-## Test 1:
+# Test 1:
 
-source_1 = Attenuated_Laser(0.48, 0)
+source_1 = Attenuated_Laser(mean_photon_number=0.48, repetition_rate=0)
 
-detector_1 = Threshold_detector(0.17,5/100,10**(-5),0) #Y_0 = 1.7*10**(-6)
+detector_1 = Threshold_detector(dark_count_rate=0.17, efficiency=5/100, time_window=10**(-5), after_pulsing=0) #Y_0 = 1.7*10**(-6)
 
-receiver_1 = Receiver(0.9)
+receiver_1 = Receiver(transmittance=0.9)
 
-channel_1 = FiberChannel(0.21,0.934)
+FiberChannel_1 = FiberChannel(loss_per_km=0.21, distance_km=0, detection_error=0.033)
 
 f_1 = 1.22
 
-#key_rate_distance_km_bb84(0,160,300,source_1,detector_1,channel_1,receiver_1,f_1,"Evolution of the key rate with the distance_km for an attenuated laser")
-
-#graph_proba(0,25,source_1, "Attenuated laser statistic")
+key_rate_distance_km_bb84(min=0, max=160, values_number=300, source=source_1, detector=detector_1, channel=FiberChannel_1, receiver=receiver_1, correction_efficiency=f_1, title="Evolution of the key rate with the distance_km for an attenuated laser")
 
 
-## Test 2:
+# Test 2:
 
-source_2 = Symmetric_Multiplexed_Heralded_Photon_Source(0.48,0,32, 0.5, 0.7)
+source_2 = Symmetric_Multiplexed_Heralded_Photon_Source(mean_photon_number=0.48, repetition_rate=0, sources_num=32, transmittance=0.5, efficiency=0.7)
 
-detector_2 = Threshold_detector(20,0.25,10**(-8),0)
+detector_2 = Threshold_detector(dark_count_rate=20, efficiency=0.25, time_window=10**(-8), after_pulsing=0)
 
-receiver_2 = Receiver(1)
+receiver_2 = Receiver(transmittance=1)
 
-channel_2 = FiberChannel(0.2,0.99)
+FiberChannel_2 = FiberChannel(loss_per_km=0.2, distance_km=0, detection_error=0.005)
 
 f_2 = 1.05
 
-#key_rate_loss_bb84(0,275,300,source_2,detector_2,channel_2,receiver_2,f_2,"Evolution of the key rate with the loss for SMHPS")
-
-#graph_proba(0,25,source_2, "SMHPS statistic")
+key_rate_loss_bb84(min=0, max=275, values_number=300, source=source_2, detector=detector_2, channel=FiberChannel_2, receiver=receiver_2, correction_efficiency=f_2, title="Evolution of the key rate with the loss for SMHPS")
 
 
-## Test 3:
+# Test 3:
 
-source_3 = Asymmetric_Multiplexed_Heralded_Photon_Source(0.6,0,32, 0.5, 0.7)
+source_3 = Asymmetric_Multiplexed_Heralded_Photon_Source(mean_photon_number=0.6, repetition_rate=0, sources_num=32, transmittance=0.5, efficiency=0.7)
 
-detector_3 = Threshold_detector(20,0.25,10**(-8),0)
+detector_3 = Threshold_detector(dark_count_rate=20, efficiency=0.25, time_window=10**(-8), after_pulsing=0)
 
-receiver_3 = Receiver(1)
+receiver_3 = Receiver(transmittance=1)
 
-channel_3 = FiberChannel(0.2,0.99)
+FiberChannel_3 = FiberChannel(loss_per_km=0.2, distance_km=0, detection_error=0.005)
 
 f_3 = 1.05
 
-#key_rate_loss_bb84(0,275,300,source_3,detector_3,channel_3,receiver_3,f_3,"Evolution of the key rate with the loss for AMHPS")
+key_rate_loss_bb84(min=0, max=275, values_number=300, source=source_3, detector=detector_3, channel=FiberChannel_3, receiver=receiver_3, correction_efficiency=f_3, title="Evolution of the key rate with the loss for AMHPS")
 
-#graph_proba(0,25,source_3, "AMHPS statistic")
 
-## Test 4:
+# Test 4:
 
-source_4 = Entangled_PDC_Source(0.053, 0)
+source_4 = Entangled_PDC_Source(mean_photon_number=0.053, repetition_rate=0)
 
-detector_4 = Threshold_detector(6.02,14.5/100,10**(-6),0)
+detector_4 = Threshold_detector(dark_count_rate=6.02, efficiency=14.5/100, time_window=10**(-6), after_pulsing=0)
 
-receiver_4 = Receiver(1)
+receiver_4 = Receiver(transmittance=1)
 
-channel_4 = FiberChannel(0.21,0.97)
+FiberChannel_4 = FiberChannel(loss_per_km=0.21, distance_km=0, detection_error=0.015)
 
 f_4 = 1.22
 
-#key_rate_loss_bbm92(0,170,300, source_4, detector_4, detector_4, channel_4, channel_4, receiver_4, receiver_4, f_4, "Evolution of the key rate with the loss for an entangled PDC source")
-
-#graph_proba(0,25,source_4, "Entangles PDC source statistic")
+key_rate_loss_pulsed_bbm92(min=0, max=170, values_number=300, source=source_4, detector1=detector_4, detector2=detector_4, channel_1=FiberChannel_4, receiver1=receiver_4, receiver2=receiver_4, correction_efficiency=f_4, title="Evolution of the key rate with the loss for an entangled PDC source")
